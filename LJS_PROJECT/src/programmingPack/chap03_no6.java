2f64e7985f8c7803ac1f55809346336a96031b2b
이지상 <leejisang99@naver.com>
2022-10-10 20:53:46 +0900

    자바 프로그래밍문제 123쪽 6번부터 풀기

---
+++ LJS_PROJECT/src/programmingPack/chap03_no6.java
@@ -0,0 +1,23 @@
+package programmingPack;
+
+import java.util.Scanner;
+
+public class chap03_no6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String chul, yong;
+		
+		System.out.print("Ã¶¼ö : ");
+		chul = in.next();
+		System.out.print("¿µÈñ : ");
+		yong = in.next();
+		
+		if (chul.equals("r") && yong.equals("r"))
+	 		System.out.println("Ã¶¼ö,¿µÈñ ¹«½ÂºÎ");
+		else if ()
+		
+	}
+
+}
