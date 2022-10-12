150896a884b2cdc7d0790c148b7f0b872b71c25d
Jisang Lee <leejisang99@naver.com>
2022-10-12 09:42:51 +0900

    자바프로그래밍 3장 6번문제까지 풀었음

--- LJS_PROJECT/src/programmingPack/chap03_no6.java
+++ LJS_PROJECT/src/programmingPack/chap03_no6.java
@@ -6,18 +6,40 @@ public class chap03_no6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s1, s2;
 		Scanner in = new Scanner(System.in);
-		String chul, yong;
-		
 		System.out.print("Ã¶¼ö : ");
-		chul = in.next();
+		s1 = in.next();
 		System.out.print("¿µÈñ : ");
-		yong = in.next();
-		
-		if (chul.equals("r") && yong.equals("r"))
-	 		System.out.println("Ã¶¼ö,¿µÈñ ¹«½ÂºÎ");
-		else if ()
-		
+		s2 = in.next();
+
+//		¹«½ÂºÎ°¡ ¾Æ´Ñ°æ¿ì
+		if (!s1.equals(s2)) {
+			if (s1.equals("r")) {
+				if (s2.equals("p"))
+					System.out.println("¿µÈñ, ½Â!");
+				else if (s2.equals("s"))
+					System.out.println("Ã¶¼ö, ½Â!");
+			}
+
+			else if (s1.equals("p")) {
+				if (s2.equals("r"))
+					System.out.println("Ã¶¼ö, ½Â!");
+				else if (s2.equals("s"))
+					System.out.println("¿µÈñ, ½Â!");
+			}
+
+			else if (s1.equals("s")) {
+				if (s2.equals("p"))
+					System.out.println("Ã¶¼ö, ½Â!");
+				else if (s2.equals("r"))
+					System.out.println("¿µÈñ, ½Â!");
+			} else
+				System.out.println("Àß¸øµÈ ÀÔ·Â°ªÀÔ´Ï´Ù.");
+		}
+//		¹«½ÂºÎÀÎ °æ¿ì
+		else
+			System.out.println("¹«½ÂºÎÀÔ´Ï´Ù! ´Ù½ÃÇÏ¼¼¿ä.");
 	}
 
 }
